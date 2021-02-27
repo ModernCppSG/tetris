6f3b2c8448855d297e2e64b8d6c4c6e979fc83c4
Aocci <tiago.oliva.costa@gmail.com>
2021-02-27 20:03:23 -0300

    feat: class Tetrimino

---
+++ tetriminos.h
@@ -0,0 +1,16 @@
+// General Tetriminos class and its ramifications
+// Created by Caju on 2021-02-27
+//
+
+#include <iostream>
+
+class Tetrimino {
+    public:
+        int numberOfBlocks = 4;
+        // Print block `drawing` to terminal using `*` for debugging
+        void display(){
+            std::cout << "Block structure";
+        };
+
+};
+
