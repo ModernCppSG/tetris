e65f7831c8fb2d21aabdc209c3bcda90d58df52e
Aocci <tiago.oliva.costa@gmail.com>
2021-02-27 20:11:53 -0300

    feat: pieces' taxonomy by inheritance

--- tetriminos.h
+++ tetriminos.h
@@ -5,12 +5,35 @@
 #include <iostream>
 
 class Tetrimino {
-    public:
-        int numberOfBlocks = 4;
-        // Print block `drawing` to terminal using `*` for debugging
-        void display(){
-            std::cout << "Block structure";
-        };
-
+public:
+  int numberOfBlocks = 4;
+  // Print block `drawing` to terminal using `*` for debugging
+  void display() { std::cout << "Block structure"; };
 };
 
+// Divide Blocks into two main families:
+//  * Symmetric Pieces, e.g., T, Square, I
+//  * Non-Symmetric Pieces: L and Z
+class SymmetricTet : private Tetrimino {};
+
+class Square : public SymmetricTet {};
+
+class Line : public SymmetricTet {};
+
+class Tee : public SymmetricTet {};
+
+class NonSymmetricTet : private Tetrimino {};
+
+class Ls : private NonSymmetricTet {};
+
+class Zs : private NonSymmetricTet {};
+
+// Non-Symmetric pieces are further specialized into their left and right
+// versions
+class LeftL : public Ls {};
+
+class RightL : public Ls {};
+
+class LeftZ : public Ls {};
+
+class RightZ : public Ls {};
