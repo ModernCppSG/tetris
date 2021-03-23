099e9e0dec2cffb15153dc73f63bf58431b90d67
Aocci <tiago.oliva.costa@gmail.com>
2021-03-22 22:44:05 -0300

    refactor: move frames definition to main.

--- main.cpp
+++ main.cpp
@@ -1,2 +1,9 @@
-#include "./include/gtest/"
-int main() { RUN_ALL_TESTS() }
+#include "gtest"
+#include "refframes.h"
+
+int main() {
+  RUN_ALL_TESTS();
+  ReferenceFrame local(5, 21);  // Block starts at MIDLE-TOP of playing field
+  ReferenceFrame GLOBAL(0, 0);
+  ReferenceFrame PRINTING(0, 20);  // TODO(aocci): flip the printing Frame down
+}
