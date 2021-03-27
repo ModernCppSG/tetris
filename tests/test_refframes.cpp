8f1dd85776a4fe5b2aae847e59ffddbd08752a92
Aocci <tiago.oliva.costa@gmail.com>
2021-03-27 14:49:07 -0300

    feat: implement clockwise rotation tests

--- tests/test_refframes.cpp
+++ tests/test_refframes.cpp
@@ -9,21 +9,46 @@
 
 std::array<std::array<float, 2>, 2> IDENTITY = {{{1, 0}, {0, 1}}};
 
+// Rotate left (counter clockwise)
 ReferenceFrame test(0, 0);
 TEST(TestReferenceFrames, CounterClockwiseRotation) {
   matrix rot090 = {{{0, -1}, {1, 0}}};
   matrix rot180 = {{{-1, 0}, {0, -1}}};
   matrix rot270 = {{{0, 1}, {-1, 0}}};
+  // Reference frame should be initialized as identity
+  ASSERT_EQ(test.orientation, IDENTITY);
+  // Rotate then compare
   test.rotate('L');
   ASSERT_EQ(test.orientation, rot090);
+  // Repeat three times
   test.rotate('L');
   ASSERT_EQ(test.orientation, rot180);
   test.rotate('L');
   ASSERT_EQ(test.orientation, rot270);
   test.rotate('L');
+  // Finally, the refframe should have gone back to the initial state
   ASSERT_EQ(test.orientation, IDENTITY);
 }
-// Rotate right (counter clockwise)
+
+// Rotate right (clockwise)
+TEST(TestReferenceFrames, ClockwiseRotation) {
+  matrix minus_rot090 = {{{0, 1}, {-1, 0}}};
+  matrix minus_rot180 = {{{-1, 0}, {0, -1}}};
+  matrix minus_rot270 = {{{0, -1}, {1, 0}}};
+  // Reference frame should be initialized as identity
+  ASSERT_EQ(test.orientation, IDENTITY);
+  // Rotate then compare
+  test.rotate('R');
+  ASSERT_EQ(test.orientation, minus_rot090);
+  // Repeat three times
+  test.rotate('R');
+  ASSERT_EQ(test.orientation, minus_rot180);
+  test.rotate('R');
+  ASSERT_EQ(test.orientation, minus_rot270);
+  test.rotate('R');
+  // Finally, the refframe should have gone back to the initial state
+  ASSERT_EQ(test.orientation, IDENTITY);
+}
 //
 //
 // Rotate left, then right
