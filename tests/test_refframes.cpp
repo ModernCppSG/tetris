b167b3571673eab67f3d658cd71a1fae1e70d546
Aocci <tiago.oliva.costa@gmail.com>
2021-03-26 23:20:30 -0300

    wip: add reference frame test

--- tests/test_refframes.cpp
+++ tests/test_refframes.cpp
@@ -1,10 +1,16 @@
 // Copyright (c) 2021 ModernCppSG
 // Testing procedures for refframes.h module
 // Created by Caju on 2021-03-07
-//
 
-// Rotate left (clockwise)
-//
+#include <array>
+
+#include "../refframes.h"
+#include "./gtest/gtest.h"
+
+std::array<std::array<float, 2>, 2> IDENTITY = {{{1, 0}, {0, 1}}};
+
+ReferenceFrame test(0, 0);
+TEST(TestReferenceFrames, ClockwiseRotation) { test.rotate('L'); }
 // Rotate right (counter clockwise)
 //
 //
@@ -13,8 +19,6 @@
 // Rotate right then left
 //
 //
-#include "./gtest/gtest.h"
-#include "./refframes.h"
 // Rotate left 4 times should return to the starting condition.
 // In other words: 90 + 90 + 90 +90 = 360 degree rotation which is identic to
 // 0 degree "rotation".
