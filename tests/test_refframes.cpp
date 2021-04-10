9e40b7f32f1f6e18138205c4a51d6d056bf8f59e
Aocci <tiago.oliva.costa@gmail.com>
2021-04-10 12:18:31 -0300

    refactor: break tests into units

--- tests/test_refframes.cpp
+++ tests/test_refframes.cpp
@@ -19,59 +19,95 @@ matrix minus_rot180 = {{{-1, 0}, {0, -1}}};
 matrix minus_rot270 = {{{0, -1}, {1, 0}}};
 
 // Rotate left (counter clockwise)
-TEST(TestReferenceFrames, CounterClockwiseRotation) {
+TEST(CounterClockWise, Initialization) {
   ReferenceFrame test(0, 0);
   // Reference frame should be initialized as identity
   ASSERT_EQ(test.orientation, IDENTITY);
+}
+
+TEST(CounterClockWise, SingleRotation) {
+  ReferenceFrame test(0, 0);
   // Rotate then compare
   test.rotate('L');
   ASSERT_EQ(test.orientation, rot090);
-  // Repeat three times
+}
+TEST(CounterClockWise, DoubleRotation) {
+  ReferenceFrame test(0, 0);
+  test.rotate('L');
   test.rotate('L');
   ASSERT_EQ(test.orientation, rot180);
+}
+TEST(CounterClockWise, TripleRotation) {
+  ReferenceFrame test(0, 0);
+  test.rotate('L');
+  test.rotate('L');
   test.rotate('L');
   ASSERT_EQ(test.orientation, rot270);
+}
+TEST(CounterClockWise, QuadrupleRotation) {
+  ReferenceFrame test(0, 0);
+  test.rotate('L');
+  test.rotate('L');
+  test.rotate('L');
   test.rotate('L');
   // Finally, the refframe should have gone back to the initial state
   ASSERT_EQ(test.orientation, IDENTITY);
 }
 
 // Rotate right (clockwise)
-TEST(TestReferenceFrames, ClockwiseRotation) {
+TEST(ClockWise, SingleRotation) {
   ReferenceFrame test(0, 0);
-  // Reference frame should be initialized as identity
-  ASSERT_EQ(test.orientation, IDENTITY);
   // Rotate then compare
   test.rotate('R');
   ASSERT_EQ(test.orientation, minus_rot090);
-  // Repeat three times
+}
+TEST(ClockWise, DoubleRotation) {
+  ReferenceFrame test(0, 0);
+  test.rotate('R');
   test.rotate('R');
   ASSERT_EQ(test.orientation, minus_rot180);
+}
+TEST(ClockWise, TripleRotation) {
+  ReferenceFrame test(0, 0);
+  test.rotate('R');
+  test.rotate('R');
   test.rotate('R');
   ASSERT_EQ(test.orientation, minus_rot270);
+}
+TEST(ClockWise, QuadrupleRotation) {
+  ReferenceFrame test(0, 0);
+  test.rotate('R');
+  test.rotate('R');
+  test.rotate('R');
   test.rotate('R');
   // Finally, the refframe should have gone back to the initial state
   ASSERT_EQ(test.orientation, IDENTITY);
 }
 
-TEST(TestReferenceFrames, MixedRotations) {
-  // Rotate left, then right
+TEST(MixedRotations, LeftRight) {
   ReferenceFrame testL(0, 0);
   testL.rotate('L');
   testL.rotate('R');
   ASSERT_EQ(testL.orientation, IDENTITY);
+}
 
+TEST(MixedRotations, LeftLeftRight) {
+  ReferenceFrame testL(0, 0);
   testL.rotate('L');
   testL.rotate('L');
   testL.rotate('R');
   ASSERT_EQ(testL.orientation, rot090);
+}
 
-  // Rotate right, then left
+TEST(MixedRotations, RightLeft) {
   ReferenceFrame testR(0, 0);
   testR.rotate('R');
   testR.rotate('L');
   ASSERT_EQ(testR.orientation, IDENTITY);
+}
 
+TEST(MixedRotations, RightRightLeft) {
+  ReferenceFrame testR(0, 0);
   testR.rotate('R');
   testR.rotate('R');
   testR.rotate('L');
