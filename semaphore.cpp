af173a5ca7d3bebcc01c438ecb717f30eacd6179
griloHBG <henriqueborgesgarcia@gmail.com>
2021-04-18 10:24:28 -0300

    There is no semaphore on my version of g++ (nor clang++) so now we have one

---
+++ semaphore.cpp
@@ -0,0 +1,5 @@
+//
+// Created by grilo on 17/04/2021.
+//
+
+#include "semaphore.h"
---
+++ semaphore.h
@@ -0,0 +1,44 @@
+//
+// Created by grilo on 17/04/2021.
+// From here: https://riptutorial.com/cplusplus/example/30142/semaphore-cplusplus-11
+//
+
+#ifndef TETRIS_SEMAPHORE_H
+#define TETRIS_SEMAPHORE_H
+
+
+#include <mutex>
+#include <condition_variable>
+
+class semaphore {
+public:
+    semaphore (int count_ = 0)
+            : count(count_)
+    {
+    }
+    
+    inline void notify( int tid ) {
+        std::unique_lock<std::mutex> lock(mtx);
+        count++;
+        //cout << "thread " << tid <<  " notify" << endl;
+        //notify the waiting thread
+        cv.notify_one();
+    }
+    inline void wait( int tid ) {
+        std::unique_lock<std::mutex> lock(mtx);
+        while(count == 0) {
+            //cout << "thread " << tid << " wait" << endl;
+            //wait on the mutex until notify is called
+            cv.wait(lock);
+            //cout << "thread " << tid << " run" << endl;
+        }
+        count--;
+    }
+private:
+    std::mutex mtx;
+    std::condition_variable cv;
+    int count;
+};
+
+
+#endif //TETRIS_SEMAPHORE_H
