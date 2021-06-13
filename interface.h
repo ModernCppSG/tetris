0f29f2f6e5ba32d482aaa5f3a6ac3876a16919bd
Aocci <tiago.oliva.costa@gmail.com>
2021-06-13 13:01:25 -0300

    (style) apply clang-format

--- interface.h
+++ interface.h
@@ -1,97 +1,95 @@
 #ifndef TETRIS_INTERFACE_H
 #define TETRIS_INTERFACE_H
 
-#include <iostream>
 #include <cstdio>
+#include <iostream>
 #include <string>
 
-//each \033[ is an ANSI escape sequence
-//class Size is used to define the dimensions of all walls in the game interface
+// each \033[ is an ANSI escape sequence
+// class Size is used to define the dimensions of all walls in the game
+// interface
 class Size {
-public:
-    Size(int width, int height): width_{width}, height_{height} {}
-    int width() const { return width_; }
-    int height() const { return height_; }
+ public:
+  Size(int width, int height) : width_{width}, height_{height} {}
+  int width() const { return width_; }
+  int height() const { return height_; }
 
-private:
-    int width_;
-    int height_;
+ private:
+  int width_;
+  int height_;
 };
 
-//prepInterface clears the terminal
-void prepInterface() {
-    std::cout << "\033[2J";
-}
+// prepInterface clears the terminal
+void prepInterface() { std::cout << "\033[2J"; }
 
-//refCursor positions the cursor at any coordinate of the terminal
+// refCursor positions the cursor at any coordinate of the terminal
 void refCursor(const std::string& x, const std::string& y) {
-    std::cout << "\033[H"; //positions the cursor at the origin
-    std::cout << "\033[" + x + "C"; //positions the cursor at the x-column
-    std::cout << "\033[" + y + "B"; //positions the cursor at the y-line
+  std::cout << "\033[H";           // positions the cursor at the origin
+  std::cout << "\033[" + x + "C";  // positions the cursor at the x-column
+  std::cout << "\033[" + y + "B";  // positions the cursor at the y-line
 }
 
-//printWalls uses the Size as an input for printing the walls at the current cursor position line by line
-//each \u25?? is a different ASCII symbol used in the wall
+// printWalls uses the Size as an input for printing the walls at the current
+// cursor position line by line each \u25?? is a different ASCII symbol used in
+// the wall
 void printWalls(const Size& walls) {
-    //beggin by printing the first line of the wall
-    std::cout << "\u2554";
-    for (int i = 0; i < walls.width(); ++i) {
-        std::cout << "\u2550";
-    }
-    std::cout << "\u2557";
+  // beggin by printing the first line of the wall
+  std::cout << "\u2554";
+  for (int i = 0; i < walls.width(); ++i) {
+    std::cout << "\u2550";
+  }
+  std::cout << "\u2557";
 
-    //print, line by line, the walls that are in the middle
-    for (int i = 0; i < walls.height(); ++i) {
-        std::cout << "\033[1B" << "\033[" + std::to_string(walls.width() + 2) + "D"; //repositioning of the cursor to be underneath of the above line
-        std::cout << "\u2551" << "\033[" + std::to_string(walls.width()) + "C" << "\u2551";
-    }
+  // print, line by line, the walls that are in the middle
+  for (int i = 0; i < walls.height(); ++i) {
+    std::cout << "\033[1B"
+              << "\033[" + std::to_string(walls.width() + 2) +
+                     "D";  // repositioning of the cursor to be underneath of
+                           // the above line
+    std::cout << "\u2551"
+              << "\033[" + std::to_string(walls.width()) + "C"
+              << "\u2551";
+  }
 
-    //print the last line of the wall
-    std::cout << "\033[1B" << "\033[" + std::to_string(walls.width() + 2) + "D";
-    std::cout << "\u255A";
-    for (int i = 0; i < walls.width(); ++i) {
-        std::cout << "\u2550";
-    }
-    std::cout << "\u255D";
-
+  // print the last line of the wall
+  std::cout << "\033[1B"
+            << "\033[" + std::to_string(walls.width() + 2) + "D";
+  std::cout << "\u255A";
+  for (int i = 0; i < walls.width(); ++i) {
+    std::cout << "\u2550";
+  }
+  std::cout << "\u255D";
 }
 
 void moveLeft();
 
 void clearPosition() {
-    std::cout << " ";
-    moveLeft();
+  std::cout << " ";
+  moveLeft();
 }
 
 void setPosition() {
-    std::cout << "#";
-    moveLeft();
+  std::cout << "#";
+  moveLeft();
 }
 
-void moveUp() {
-    std::cout << "\033[1A";
-}
+void moveUp() { std::cout << "\033[1A"; }
 
-void moveDown() {
-    std::cout << "\033[1B";
-}
+void moveDown() { std::cout << "\033[1B"; }
 
-void moveRight() {
-    std::cout << "\033[1C";
-}
+void moveRight() { std::cout << "\033[1C"; }
 
-void moveLeft() {
-    std::cout << "\033[1D";
-}
+void moveLeft() { std::cout << "\033[1D"; }
 
 void printCerquilha(int xOld, int yOld, int x, int y) {
-    std::cout << "\033[0;0f";
-    std::cout << "   x: " << x << "\t   y: " << y << std::endl;
-    std::cout << "xOld: " << xOld << "\tyOld: " << yOld;
-    std::cout << "\033[" + std::to_string(yOld) << ";" << std::to_string(xOld) << "f";
-    std::cout << "o";
-    std::cout << "\033[" + std::to_string(y) << ";" << std::to_string(x) << "f";
-    std::cout << "#";
+  std::cout << "\033[0;0f";
+  std::cout << "   x: " << x << "\t   y: " << y << std::endl;
+  std::cout << "xOld: " << xOld << "\tyOld: " << yOld;
+  std::cout << "\033[" + std::to_string(yOld) << ";" << std::to_string(xOld)
+            << "f";
+  std::cout << "o";
+  std::cout << "\033[" + std::to_string(y) << ";" << std::to_string(x) << "f";
+  std::cout << "#";
 }
 
-#endif //TETRIS_INTERFACE_H
+#endif  // TETRIS_INTERFACE_H
--- internal_clock.hpp
+++ internal_clock.hpp
@@ -8,44 +8,40 @@
 #ifndef INTERNAL_CLOCK_HPP_
 #define INTERNAL_CLOCK_HPP_
 
-#include <thread>
-#include <iostream>
-#include <chrono>
 #include <atomic>
+#include <chrono>
+#include <iostream>
+#include <thread>
 
 using time_unit = std::chrono::milliseconds;
 
 class internal_clock {
+ private:
+  std::chrono::time_point<std::chrono::system_clock> start;
+  time_unit interval;
 
-private:
+  std::thread my_thread;
+  std::atomic<bool>
+      is_interval_elapsed;  // Might want to use condition_variable
+  std::atomic_flag stop_clock = ATOMIC_FLAG_INIT;
+  std::atomic<uint32_t> elapsed_intervals;  // this MIGHT overflow
 
-	std::chrono::time_point<std::chrono::system_clock> start;
-	time_unit interval;
+ public:
+  internal_clock(time_unit x);
+  void reset();
+  void set_interval(time_unit x);
+  time_unit get_interval();
+  void print_elapsed();
+  time_unit get_elapsed();
 
-	std::thread my_thread;
-	std::atomic<bool> is_interval_elapsed;// Might want to use condition_variable
-	std::atomic_flag stop_clock = ATOMIC_FLAG_INIT;
-	std::atomic<uint32_t> elapsed_intervals; // this MIGHT overflow
+  bool get_is_elapsed();
+  unsigned int get_elapsed_intervals();
 
-public:
+  ~internal_clock();
 
-	internal_clock(time_unit x);
-	void reset();
-	void set_interval(time_unit x);
-	time_unit get_interval();
-	void print_elapsed();
-	time_unit get_elapsed();
-
-	bool get_is_elapsed();
-	unsigned int get_elapsed_intervals();
-
-	~internal_clock();
-
-	void run(int interval_slice, int firing_slice);
-	void terminate();
-	void instructions(int interval_slice, int firing_slice);
-
+  void run(int interval_slice, int firing_slice);
+  void terminate();
+  void instructions(int interval_slice, int firing_slice);
 };
-
 
 #endif /* INTERNAL_CLOCK_HPP_ */
